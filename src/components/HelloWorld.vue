c8d7e2e5e69579457c10bf75032b0c0a983a7b6b
Christina Huang <70524046+ch3427@users.noreply.github.com>
2023-11-23 13:07:24 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="elearning-container">
     <h1>{{ msg }}</h1>
 
     <p>
@@ -27,11 +27,13 @@
     <button type="button" @click="count++">Count is: {{ count }}</button>
 
     <!-- Chat button -->
-    <button @click="openChat">Open Chat</button>
+    <button @click="toggleChat" class="chat-button">Open Chat</button>
 
     <!-- Chat popup -->
     <div v-if="isChatOpen" class="chat-popup">
-      <!-- Replace the alert with your actual chat component -->
+      <!-- Replace the input and button with your actual chat component -->
+      <input v-model="chatMessage" placeholder="Type your message..." />
+      <button @click="sendChatMessage">Send</button>
       <p @click="closeChat">Chat with fellow learners!</p>
     </div>
 
@@ -52,19 +54,50 @@ defineProps({
 
 const count = ref(0)
 const isChatOpen = ref(false)
+const chatMessage = ref('')
 
-const openChat = () => {
-  isChatOpen.value = true
+const toggleChat = () => {
+  isChatOpen.value = !isChatOpen.value
 }
 
 const closeChat = () => {
   isChatOpen.value = false
 }
+
+const sendChatMessage = () => {
+  // Replace this with your actual chat logic
+  alert(`Sending message: ${chatMessage.value}`);
+  chatMessage.value = '';
+}
 </script>
 
 <style scoped>
-a {
-  color: #42b983;
+/* Global styles */
+body {
+  font-family: 'Arial', sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f5f5f5;
+}
+
+/* Container styles */
+.elearning-container {
+  max-width: 800px;
+  margin: 20px auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+/* Chat button styles */
+.chat-button {
+  background-color: #42b983;
+  color: #fff;
+  padding: 10px;
+  border: none;
+  cursor: pointer;
+  border-radius: 4px;
 }
 
 /* Chat popup styles */
@@ -72,10 +105,26 @@ a {
   position: fixed;
   bottom: 20px;
   right: 20px;
-  background-color: #f1f1f1;
+  background-color: #fff;
   border: 1px solid #ccc;
   padding: 10px;
-  z-index: 1;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+/* Chat input and button styles */
+input {
+  padding: 8px;
+  margin-right: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  background-color: #42b983;
+  color: #fff;
+  border: none;
+  padding: 8px;
+  border-radius: 4px;
   cursor: pointer;
 }
 </style>
