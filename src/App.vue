cef34588b4db965766ca35e1183be5feaee6665e
Christina Huang <70524046+ch3427@users.noreply.github.com>
2023-11-23 14:40:59 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,29 +6,36 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to our Art Gallery" />
+    <div class="header">
+      <HelloWorld msg="Welcome to our Art Gallery" />
+    </div>
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Merriweather', serif; /* Change to your preferred font for an editorial look */
+  font-family: 'Merriweather', serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #333; /* Dark text color for a classic editorial feel */
-  background-color: #f5f5f5; /* Light background color */
-  padding: 80px 20px; /* Increased padding for more space */
-  max-width: 800px; /* Limit content width for a magazine-style layout */
-  margin: 0 auto; /* Center the content */
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1); /* Add a subtle shadow for depth */
+  background-color: #f9f9f9;
+  padding: 80px 20px;
+  max-width: 800px;
+  margin: 0 auto;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+}
+
+.header {
+  background-color: #c72d4d;
+  padding: 20px;
+  border-radius: 10px;
 }
 
 /* Style the HelloWorld component for a cosmetic brand or art gallery look */
 HelloWorld {
-  font-size: 2em; /* Larger font size for emphasis */
-  margin-top: 40px; /* Increased margin for better spacing */
-  color: #c72d4d; /* Cosmetic brand color or a color associated with art */
+  font-size: 2em;
+  margin-top: 20px;
+  color: #fff; /* Text color on the colored background */
   font-style: italic;
 }
 </style>
