9dc8d4c73175b437ae05c84a981bcea19b1bfc30
Christina Huang <70524046+ch3427@users.noreply.github.com>
2023-11-23 14:38:51 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3 + Vite" />
+    <HelloWorld msg="Welcome to our Art Gallery" />
   </div>
 </template>
 
@@ -19,22 +18,17 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #333; /* Dark text color for a classic editorial feel */
   background-color: #f5f5f5; /* Light background color */
-  padding: 60px 0; /* Adjust padding as needed */
+  padding: 80px 20px; /* Increased padding for more space */
   max-width: 800px; /* Limit content width for a magazine-style layout */
   margin: 0 auto; /* Center the content */
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.1); /* Add a subtle shadow for depth */
 }
 
-img {
-  max-width: 100%;
-  height: auto;
-}
-
-/* Style the HelloWorld component for an editorial touch */
+/* Style the HelloWorld component for a cosmetic brand or art gallery look */
 HelloWorld {
-  font-size: 1.5em;
-  margin-top: 20px;
-  color: #444;
+  font-size: 2em; /* Larger font size for emphasis */
+  margin-top: 40px; /* Increased margin for better spacing */
+  color: #c72d4d; /* Cosmetic brand color or a color associated with art */
   font-style: italic;
 }
 </style>
