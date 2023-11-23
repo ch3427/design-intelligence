96846d66b4d840c74a38f6def4b9b95b61532ed7
Christina Huang <70524046+ch3427@users.noreply.github.com>
2023-11-23 13:11:42 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="landing-page">
-    <!-- Header Section -->
     <header>
       <h1>Learn Lean Product Management</h1>
       <p>Unlock the secrets of efficient product development with lean principles.</p>
     </header>
 
-    <!-- Introduction Section -->
     <section class="introduction">
       <h2>Introduction</h2>
       <p>
-        Welcome to our Lean Product Management course! Whether you're a seasoned product manager or just starting, 
+        Welcome to our Lean Product Management course! Whether you're a seasoned product manager or just starting,
         this course will equip you with the skills and knowledge to streamline your product development process.
       </p>
     </section>
 
-    <!-- Key Concepts Section -->
     <section class="key-concepts">
       <h2>Key Concepts</h2>
       <ul>
@@ -27,55 +24,42 @@
       </ul>
     </section>
 
-    <!-- Resources Section -->
     <section class="resources">
       <h2>Resources</h2>
       <p>Explore these resources to deepen your understanding of Lean Product Management:</p>
       <ul>
         <li><a href="https://www.lean.org/" target="_blank">Lean Enterprise Institute</a></li>
         <li><a href="https://leanstack.com/" target="_blank">Lean Stack</a></li>
-        <li><a href="https://www.amazon.com/Lean-Startup-Entrepreneurs-Continuous-Innovation/dp/0307887898" target="_blank">The Lean Startup by Eric Ries</a></li>
+        <li><a
+            href="https://www.amazon.com/Lean-Startup-Entrepreneurs-Continuous-Innovation/dp/0307887898"
+            target="_blank">The Lean Startup by Eric Ries</a></li>
         <!-- Add more resources as needed -->
       </ul>
     </section>
 
-    <!-- Call to Action Section -->
     <section class="cta">
       <h2>Ready to Get Started?</h2>
       <p>Enroll now and take the first step towards becoming a Lean Product Management expert!</p>
-      <button>Enroll Now</button>
+      <button @click="enrollNow">Enroll Now</button>
     </section>
 
-    <!-- Footer Section -->
     <footer>
       <p>&copy; 2023 Lean Product Management Institute. All rights reserved.</p>
     </footer>
   </div>
 </template>
 
+<script>
+export default {
+  methods: {
+    enrollNow() {
+      // Implement your enrollment logic here
+      alert('Enrollment logic goes here!');
+    }
+  }
+};
+</script>
+
 <style scoped>
-/* Global styles */
-body {
-  font-family: 'Arial', sans-serif;
-  margin: 0;
-  padding: 0;
-  background-color: #f5f5f5;
-}
-
-/* Landing page styles */
-.landing-page {
-  max-width: 800px;
-  margin: 20px auto;
-  padding: 20px;
-  background-color: #fff;
-  border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
-
-/* Header styles */
-header {
-  text-align: center;
-  margin-bottom: 20px;
-}
-
-/*
+/* Your styles go here */
+</style>
